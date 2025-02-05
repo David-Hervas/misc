2d5a48d8ed7a299b5e2c160cb9e72dac04c1776a
David Hervas <bioestadistica@iislafe.es>
2025-02-05 11:54:17 +0100

    Function to read .sgd files
    Reads StatGraphics .sgd files

---
+++ read_sgd.R
@@ -0,0 +1,21 @@
+require(XML)
+library(data.table)
+library(clickR)
+
+read_sgd <- function(file, k=8, ...){
+  data <- xmlParse(file, ...)
+  xml_data <- xmlToList(data)
+  if(any(sapply(xml_data$COLUMNS, function(x) x["Type"]) == "N")){
+    datos <- fix_factors(fix_numerics(as.data.frame(rbindlist(lapply(xml_data[names(xml_data) %in% "ROW"], function(x){
+      as.data.frame(do.call(cbind, x))
+    }), fill=TRUE))), k=k)
+  } else{
+    datos <- fix_factors(as.data.frame(rbindlist(lapply(xml_data[names(xml_data) %in% "ROW"], function(x){
+      as.data.frame(do.call(cbind, x))
+    }), fill=TRUE)), k=k)
+  }
+  names(datos) <- sapply(xml_data$COLUMNS, function(x) x["Name"])
+  datos
+}
+
+marcador <- read_sgd("W:\\datos 1p-GIB-2024-2025-B.sgd")
